d198a09a23f74961bfc3030463ffe9cba211e643
Ksenia <Kseniakuzevanova@gmail.com>
2022-11-30 16:59:37 +0700

    1

---
+++ 66задача/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 66: Задайте значения M и N. Напишите метод, 
+//который найдёт сумму натуральных элементов в промежутке от M до N. 
+//Через рекурсию!
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// int M = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[N];
+// FillArray(N, array);
+// PrintArray(array);
+
+// void FillArray(int number, int[] array, int count = 0)
+// {
+//     if (count + 1 > number)
+//     {
+//         return;
+//     }
+//     array[count] = count + 1;
+//     count++;
+//     FillArray(number, array, count);
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         Console.Write($" {i}");
+//     }
+// }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значение N. Напишите метод, 
+//который выведет все натуральные числа в промежутке от N до 1.
+//Выполнить с помощью рекурсии.
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+FillArray(number, array);
+PrintArray(array);
+
+void FillArray(int number, int[] array, int count = 0)
+{
+    if (count + 1 > number)
+    {
+        return;
+    }
+    array[count] = count + 1;
+    count++;
+    FillArray(number, array, count);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($" {i}");
+    }
+}  
